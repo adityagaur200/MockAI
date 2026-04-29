46bb74ef388c944571a718577b0d27ef9249fcd3
adityagaur200 <115742009+adityagaur200@users.noreply.github.com>
2026-04-30 04:36:02 +0530

    refactor

--- Mockmate AI/vite.config.ts
+++ Mockmate AI/vite.config.ts
@@ -3,22 +3,21 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
-// https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
+export default defineConfig({
   server: {
-    host: "::",
+    host: true,
     port: 3333,
     hmr: {
       overlay: false,
     },
   },
-  plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
+  plugins: [react(), componentTagger()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
   preview: {
-   allowedHosts:["all"]
+    allowedHosts: true,   // ✅ correct type
   },
-}));
+});
