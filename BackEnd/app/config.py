dc9a7a386bcc82a5aa2b54181dafb8d14b6f6567
adityagaur200 <115742009+adityagaur200@users.noreply.github.com>
2026-04-30 11:40:06 +0530

    chsnge config

--- BackEnd/app/config.py
+++ BackEnd/app/config.py
@@ -2,6 +2,5 @@ import os
 from dotenv import load_dotenv
 
 load_dotenv()
-
-MONGO_URI = os.getenv("MONGO_URI")
-REDIS_URL = os.getenv("REDIS_URL", "redis://localhost:6379/0")
+MONGO_URI = os.environ["MONGO_URI"]
+REDIS_URL = os.environ["REDIS_URL"]
