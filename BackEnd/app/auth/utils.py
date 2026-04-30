c67e195ebd1b52c58e4e23e43ec681f8d96ea7de
adityagaur200 <115742009+adityagaur200@users.noreply.github.com>
2026-04-30 12:07:03 +0530

    fix deploy issues

--- BackEnd/app/auth/utils.py
+++ BackEnd/app/auth/utils.py
@@ -14,11 +14,11 @@ pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 
 def hash_password(password: str):
-    return pwd_context.hash(password[:72])
+    return pwd_context.hash(password.encode('utf-8')[:72])
 
 
 def verify_password(plain_password, hashed_password):
-    return pwd_context.verify(plain_password[:72], hashed_password)
+    return pwd_context.verify(plain_password, hashed_password)
 
 
 def create_access_token(data: dict):
